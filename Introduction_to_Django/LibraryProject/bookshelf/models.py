b7bc53d46895144f36ebca338263c5cd7d6ea0ff
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-07-14 00:08:19 +0300

    Update models.py

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -7,3 +7,29 @@ class Book(models.Model):
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
+
+from bookshelf.models import Book
+book = Book(title="1984", author="George Orwell", publication_year=1949)
+book.save()
+book
+# <Book: 1984 by George Orwell (1949)>
+
+from bookshelf.models import Book
+book = Book.objects.get(title="Nineteen Eighty-Four")
+book.delete()
+Book.objects.all()
+# (1, {'bookshelf.Book': 1})
+# <QuerySet []>
+
+from bookshelf.models import Book
+Book.objects.all()
+# <QuerySet [<Book: 1984 by George Orwell (1949)>]>
+
+
+from bookshelf.models import Book
+book = Book.objects.get(title="1984")
+book.title = "Nineteen Eighty-Four"
+book.save()
+book
+# <Book: Nineteen Eighty-Four by George Orwell (1949)>
+
