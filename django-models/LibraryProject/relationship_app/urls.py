1e1c6a57bc55ec601c78f48bda9307502c221c4a
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-07-20 22:42:34 +0300

    Update urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('librarian-dashboard/', views.librarian_view, name='librarian_view'),
     path('member-dashboard/', views.member_view, name='member_view'),
    
-    path('add/', views.add_book, name='add_book'),
-    path('edit/<int:pk>/', views.edit_book, name='edit_book'),
-    path('delete/<int:pk>/', views.delete_book, name='delete_book'),
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
 ]
