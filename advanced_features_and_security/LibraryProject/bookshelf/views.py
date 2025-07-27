47e7da6cc1fd19ee0633065da5c2fc23fa53cd52
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-07-27 14:20:47 +0300

    Update views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,10 +1,12 @@
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
-from .forms import BookSearchForm
+from django.contrib.auth.decorators import permission_required
+from django.contrib import messages
+from .models import Book
+from .forms import BookForm, BookSearchForm
 
 def index(request):
     return HttpResponse("Welcome to the Book Shelf!")
-
-# Safe input handling
 
 def search_books(request):
     form = BookSearchForm(request.GET or None)
@@ -14,4 +16,23 @@ def search_books(request):
         results = Book.objects.filter(title__icontains=query)
     return render(request, 'bookshelf/book_list.html', {'form': form, 'results': results})
 
+@permission_required('bookshelf.can_create', raise_exception=True)
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Book added successfully!")
+            return redirect('index')
+    else:
+        form = BookForm()
+    return render(request, 'bookshelf/add_book.html', {'form': form})
 
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        messages.success(request, "Book deleted successfully!")
+        return redirect('index')
+    return render(request, 'bookshelf/delete_book.html', {'book': book})
