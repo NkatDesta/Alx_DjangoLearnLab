3f538e627d5678593f410f7f4c0772f16ec22125
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-07-27 14:18:00 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -27,6 +27,20 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
 
 
 
