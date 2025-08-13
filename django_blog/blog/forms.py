a3f2ee5559b69372921fa267cd517e953d091510
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-08-14 00:17:26 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -26,10 +26,12 @@ class PostForm(forms.ModelForm):
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ['content']
+        fields = ['author', 'content']
         widgets = {
             'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Add your comment...'}),
+            'author': forms.TextInput(attrs={'placeholder': 'Your name'}),
         }
         labels = {
             'content': ''
         }
+
