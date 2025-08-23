9b885d1975ce34be0f51a7452e8e792c5a83199c
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-08-24 01:44:16 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,7 +1,14 @@
-from rest_framework import generics
-from .serializers import UserRegistrationSerializer
-from rest_framework.permissions import AllowAny
+# accounts/views.py
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from .models import CustomUser
+from .serializers import CustomUserSerializer
 
-class UserRegistrationView(generics.CreateAPIView):
-    serializer_class = UserRegistrationSerializer
-    permission_classes = [AllowAny]
+class UserListView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    serializer_class = CustomUserSerializer
+
+    def get(self, request, *args, **kwargs):
+        users = CustomUser.objects.all()
+        serializer = self.get_serializer(users, many=True)
+        return Response(serializer.data)
