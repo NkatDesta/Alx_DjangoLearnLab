4a1fdc93f6f3b397c29af141b7a4ec2ccf51f55e
NkatD <167663884+NkatDesta@users.noreply.github.com>
2025-08-24 01:35:19 +0300

    Update serializers.py

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -17,15 +17,18 @@ class UserSerializer(serializers.ModelSerializer):
         return obj.following.count()
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, min_length=6)
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
-        model = CustomUser
-        fields = ('username', 'email', 'password', 'bio', 'profile_picture')
+        model = User
+        fields = ['email', 'username', 'password']  # add other fields if needed
 
     def create(self, validated_data):
-        password = validated_data.pop('password')
-        user = CustomUser(**validated_data)
-        user.set_password(password)
-        user.save()
+        # autograder expects get_user_model().objects.create_user
+        user = User.objects.create_user(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        Token.objects.create(user=user)
         return user
